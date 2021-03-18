518e176c54b0b06fb252d8c6259dbf2fe1e95c4a
attrix <lucho.bernal@hotmail.com>
2021-03-18 12:36:40 -0300

    a

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static(__dirname + '/primerapp'));
+app.use(express.static(__dirname + '/angularapp'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/primerapp/index.html'));
+    res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
 
 console.log("Build");
